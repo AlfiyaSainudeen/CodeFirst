6f401da214d0a20e76e7c98bcd94421193f03a1b
AlfiyaSainudeen <154522933+AlfiyaSainudeen@users.noreply.github.com>
2024-01-17 17:36:30 +0530

    commit_1

---
+++ AssignmentOnCodeFirst/BookServiceAPI/BookServiceAPI/Migrations/20240117083121_mig1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookServiceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookName = table.Column<string>(type: "Varchar(30)", maxLength: 30, nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Author = table.Column<string>(type: "Varchar(30)", maxLength: 30, nullable: true),
+                    Lang = table.Column<string>(type: "Varchar(30)", maxLength: 30, nullable: true),
+                    ReleaseDate = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
